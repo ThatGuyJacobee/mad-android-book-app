669e2f7ff54415c96fe1697ac1cafb90f39c42df
Jacob <42719082+ThatGuyJacobee@users.noreply.github.com>
2025-02-24 01:50:41 +0000

    tweak: search field
    + Adjust the search field with a secondary colour outline and black placeholder text.

--- app/src/main/java/com/example/mad_android_book_app/MainActivity.kt
+++ app/src/main/java/com/example/mad_android_book_app/MainActivity.kt
@@ -62,6 +62,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -231,8 +232,14 @@ fun BookListScreen(
         TextField(
             value = searchQuery,
             onValueChange = { searchQuery = it },
-            label = { Text("Search for Books by Name...") },
-            modifier = Modifier.fillMaxWidth()
+            label = { Text(
+                "Search for Books by Name...",
+                color = Color.Black
+            ) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(32.dp))
+                .border(2.dp, Color(0xFF737ADA), RoundedCornerShape(32.dp)),
         )
 
         // Button & Dropdown Menu for sorting
